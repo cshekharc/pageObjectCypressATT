a563cff2f4adca456e5cd9ced7f6fbd5577a0f47
cshekharc <cshekhars.choudhari@gmail.com>
2023-08-17 22:37:40 +0530

    changes in config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: false,
+    saveAllAttempts: false,
+    showPassed : true,
+    code: false
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    // baseUrl: "https://demo.guru99.com/insurance/v1/index.php"
+    baseUrl: "https://demo.guru99.com/insurance/v1/index.php",
     // pageLoadTimeout : 15000
     experimentalRunAllSpecs: true,
     experimentalStudio : true,
@@ -15,6 +26,10 @@ module.exports = defineConfig({
       runMode: 1,
       openMode: 2,
     },
+    env:{
+      userNamefromenv: "test@test",
+      passWordfromenv : "test123",
+    }
   },
   
 });
